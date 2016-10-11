19c632b5e25611182317aac727ceb9bac121edb1
刘宏立 <liuhongli2120@163.com>
2016-10-12 00:16:07 +0800

    准备图像

---
+++ AsyncGraphicsCorner/AsyncGraphicsCorner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AsyncGraphicsCorner
+//
+//  Created by 刘宏立 on 16/10/12.
+//  Copyright © 2016年 lhl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AsyncGraphicsCorner/AsyncGraphicsCorner/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  AsyncGraphicsCorner
+//
+//  Created by 刘宏立 on 16/10/12.
+//  Copyright © 2016年 lhl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupUI];
+}
+
+
+- (void)setupUI {
+    
+    
+    UIImageView *iv = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 200, 200)];
+    
+    iv.center = self.view.center;
+    
+    [self.view addSubview:iv];
+    
+    UIImage *image = [UIImage imageNamed:@"28"];
+    iv.image = image;
+}
+
+@end
