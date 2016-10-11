c7c829fd4860a5df6cbb552802a6ef181aa56b39
刘宏立 <liuhongli2120@163.com>
2016-10-12 00:19:38 +0800

    创建分类

---
+++ AsyncGraphicsCorner/AsyncGraphicsCorner/UIImage+Extention.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Extention.h
+//  AsyncGraphicsCorner
+//
+//  Created by 刘宏立 on 16/10/12.
+//  Copyright © 2016年 lhl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Extention)
+
+@end
---
+++ AsyncGraphicsCorner/AsyncGraphicsCorner/UIImage+Extention.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Extention.m
+//  AsyncGraphicsCorner
+//
+//  Created by 刘宏立 on 16/10/12.
+//  Copyright © 2016年 lhl. All rights reserved.
+//
+
+#import "UIImage+Extention.h"
+
+@implementation UIImage (Extention)
+
+@end
